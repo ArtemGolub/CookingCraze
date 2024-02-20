368566843dea6007bcc6e4334f18fa85ce234ed5
Artem Golub <otodraliti@gmail.com>
2024-02-20 12:41:11 +0300

    feat: ServeOrder

--- Assets/Scripts/Controllers/CustomersController.cs
+++ Assets/Scripts/Controllers/CustomersController.cs
@@ -139,7 +139,26 @@ namespace CookingPrototype.Controllers {
 		/// <param name="order">Заказ, который пытаемся отдать</param>
 		/// <returns>Флаг - результат, удалось ли успешно отдать заказ</returns>
 		public bool ServeOrder(Order order) {
-			throw  new NotImplementedException("ServeOrder: this feature is not implemented.");
+			Customer customer = GetCustomerByOrder(order);
+			if ( customer == null ) return false;
+			customer.ServeOrder(order);
+			if ( !customer.IsComplete ) return true;
+			FreeCustomer(customer);
+			return true;
+		}
+
+		Customer GetCustomerByOrder(Order order) 
+		{
+			List<Customer> sortedCustomers = CustomerPlaces
+				.Where(place => place.CurCustomer != null)
+				.Select(place => place.CurCustomer)
+				.OrderBy(customer => customer.WaitTime)
+				.ToList();
+
+			Customer targetCustomer =
+				sortedCustomers.FirstOrDefault(customer => customer.OrderPlaces.Any(orderPlace => orderPlace.CurOrder == order));
+			
+			return targetCustomer;
 		}
 	}
 }
